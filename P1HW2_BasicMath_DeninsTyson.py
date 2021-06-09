059e4508e5b0f3e60bf4b29f5c0c3cadb4acd2e1
tyson1dennis <85641499+tyson1dennis@users.noreply.github.com>
2021-06-09 14:22:27 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_DeninsTyson.py
@@ -0,0 +1,19 @@
+# This program is deigned to do basic math functions.
+# 06/09/2021
+# CTI-110-1002 P1HW2 - Basic Math
+# Tyson Dennis
+
+# The program will ask for the user to input two integers.
+# The program will then add and multiply the two numbers.
+# The program will output the sum and product of the two numbers.
+
+num1 = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
+
+sum = num1 + num2
+product = num1 * num2
+
+print("Your first number is:", num1)
+print("Your second number is:", num2)
+print("The sum of your two numbers is:", sum)
+print("The product of your two numbers is:", product)
